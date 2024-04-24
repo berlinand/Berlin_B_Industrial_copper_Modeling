c6930b1e513ac2301a5cf264862334276defe0fb
Berlin B <154864172+berlinand@users.noreply.github.com>
2024-04-24 11:17:50 +0530

    Update Homepage.py

--- project_code/Homepage.py
+++ project_code/Homepage.py
@@ -23,7 +23,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score,precision_score,f1_score,recall_score,roc_auc_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
-
+from sklearn.preprocessing import StandardScaler
 #This function is used to clean dataset by droping or filling null value and removing unwanted columns 
 def datacleaning(df):
 
@@ -66,7 +66,6 @@ def clearoutliers(df,cns,cs2,ab):
 def skewnes(af,aa):
    
   for col in aa:
-    print(col)
     kw=skew(af[col])
     if kw>-0.5 and kw<0.5:
        print(skew(af[col]))
@@ -215,6 +214,8 @@ def mlclas(models,af,cd,ch3,ch4,ch5):
             model.fit(X_train, y_train)           
             train_predict = model.predict(X_train)
             test_predict = model.predict(X_test)
+
+
             st.write(f":violet[Train Accuracy]-{accuracy_score(y_train,train_predict)}")
             st.write(f":violet[Train Precision]-{precision_score(y_train,train_predict)}")
             st.write(f":violet[Train F1] -{f1_score(y_train,train_predict)}")
@@ -225,6 +226,7 @@ def mlclas(models,af,cd,ch3,ch4,ch5):
             st.write(f":violet[Test F1 ]-{f1_score(y_test,test_predict)}")
             st.write(f":violet[Test Recall] -{recall_score(y_test,test_predict)}")
             st.write(f":violet[Test Auc] -{roc_auc_score(y_test,test_predict)}")
+            return X_test,X_train
 
  #This function give good parameter value for the different classification models  in train and test data 
 def cvse(models,af):
@@ -314,19 +316,143 @@ def mlreg(af,models,ch4,ch5):
             st.write(F":violet[Test RMSE]-{math.sqrt(mean_squared_error(y_test,test_pred))} ")
             st.write(F":violet[Train MAE]-{mean_absolute_error(y_train,train_pred)} ")
             st.write(F":violet[Test MAE]-{mean_absolute_error(y_test,test_pred)} ")
-
+            return X_test,X_train
 
 #This function to read the data file from local system and convert into dataframe
 @st.cache_data          
 def readdata():
  df=pd.read_excel(r"C:\Users\berli\Downloads\Copper_Set.xlsx",nrows=2000)
  return df
+
 #This function will return random sample data from dataframe 
 def sa_random(df):
   df=df.sample(n=rsa, random_state=1)
   return df
 
 
+#this function change the datatype of each element in the list
+def con(txt,ch4,recl):
+  tf=txt.split(',')
+  cd=[]
+
+  for x in tf:
+    x=str(x)
+    if x[0].isalpha()==True:
+      cd.append(x) 
+    elif '/' in x:
+
+      cd.append(x)
+    elif '.' in x:
+      x=float(x)
+      cd.append(x)
+    elif x.isdigit()==True:
+      x=float(x)
+      cd.append(x)
+
+    elif x.isdecimal()==True:
+        x=float(x)
+        cd.append(x)
+
+    else:   
+        cd.append(x)
+  cd[0]=float(cd[0]) 
+  if len(ch4)==0:
+    if recl=='Classification':
+       cd.insert(3,np.nan)
+    if recl=='Regression':
+       cd.insert(10,np.nan)
+  return cd
+
+#this function to standardize the newly added data in the text input 
+def sclr(X_train,X_test,X_test_new):
+ scalar=StandardScaler()
+ scalar.fit(X_train) 
+ scalar.transform(X_test)
+ X_test_new=np.array(X_test_new)
+
+ X_test_new=X_test_new.reshape(1,-1)
+ X_test_new1=scalar.transform(X_test_new)
+ return X_test_new1
+
+#This function to display predicted status won or lost
+def mlclas1(models,af,cd,ch3,ch4,ch5,X_test_new1):
+            if len(ch4)==0 or len(ch4)==1:
+              X = af.drop(['status'], axis=1)
+            else:
+              X=af[ch4]
+            y = af['status']
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ch5)
+            if models=='Logistic Regression':
+             if cd!=None:
+               model = LogisticRegression(**cd)
+             else:
+              model = LogisticRegression()
+
+            if models=='K-NN':
+             if cd!=None: 
+              model = KNeighborsClassifier(**cd)   
+             else:          
+              model = KNeighborsClassifier(n_neighbors=3)
+            if models=='svc':
+             if cd!=None:
+               model = SVC(**cd)
+             else:
+              model = SVC()
+            if models=='Decision Tree Classifier':
+             if cd!=None:
+               model = DecisionTreeClassifier(**cd)
+             else:
+              model = DecisionTreeClassifier()            
+            if models =='Random Forest Classifier':
+             if cd!=None:
+               model = RandomForestClassifier(**cd)
+             else:
+              model =RandomForestClassifier()                
+
+
+            if models =='Gradient Boosting Classifier':
+             if cd!=None:
+               model = GradientBoostingClassifier(**cd)
+             else:
+              model =GradientBoostingClassifier()  
+            model.fit(X_train, y_train)           
+            train_predict = model.predict(X_train)
+            test_predict = model.predict(X_test)
+
+            test_predict1 = model.predict(X_test_new1)
+            if test_predict1[0]==1:
+               
+               st.write(f':blue[Status]= won')
+            else:
+              st.write(f':red[Status]= Lost')
+
+#This function is to display the predicted selling price
+def mlreg1(af,models,ch4,ch5,X_test_new1):
+            if len(ch4)==0 or len(ch4)==1:         
+               X = af.drop(['selling_price'], axis=1)
+            else:
+              X=af[ch4]
+            y = af['selling_price']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ch5)
+            if models=='Linear Regression':
+               model=LinearRegression()
+            if models=='Lasso':
+               model=Lasso()
+            if models=='Ridge':
+               model=Ridge()        
+            if models=='Decision Tree Regression':
+               model=DecisionTreeRegressor()
+                  
+            if models=='Random Forest Regression':
+               model=RandomForestRegressor() 
+            if models=='Gradient Boosting Regression':
+               model=GradientBoostingRegressor()                    
+            model.fit(X_train,y_train)
+            train_pred=model.predict(X_train)
+            test_pred=model.predict(X_test)
+            test_predict1 = model.predict(X_test_new1)
+            st.write(f':blue[selling price]= {''.join(test_predict1.astype(str))}')
 
 
 df=readdata()
@@ -341,9 +467,11 @@ if ct1 ==True:
 
 df=datacleaning(df)
 aa=list(df.columns)
+print(aa)
 aa.remove('material_ref')
 aa.remove('status')
 aa.remove('item type')
+
 aa.sort()
 
 
@@ -351,15 +479,22 @@ aa.sort()
 co1,co2,co3=st.columns(3) 
 ab=co1.selectbox(label="Before or After", options=["BEFORE","AFTER"])
 ch1=co3.checkbox(label="HEATMAP")
-af=clearoutliers(df,aa,cs2,ab)
-af=skewnes(af,aa)
-af=lblencode(af)
+abcd=clearoutliers(df,aa,cs2,ab)
+af1=abcd.copy()
+af2=skewnes(af1,aa)
+
+af=lblencode(af2)
 af=onehotencode(af)
+
 chart=co2.selectbox(label="Select a Chart",options=["BOXPLOT" ,"VIOLINPLOT","DISTPLOT","HISTPLOT"])
 charts(df,aa,ab,chart,af)
 if ch1==True:
   heatmaps(af)
+
+ch3=False
 co4,co5,co6,co7,co8=st.columns(5)
+st.warning('quantity tons, customer, country, status, item type, application, thickness, width, material_ref, product_ref, selling_price')
+txt=st.text_input(label="column value") 
 recl=co4.selectbox(label="select a value",options=['Classification','Regression'])
 if recl == 'Classification':
   models=co5.selectbox(label='select a model',options=['Logistic Regression','K-NN','svc','Decision Tree Classifier',
@@ -368,17 +503,70 @@ if recl == 'Classification':
   ch4=co7.multiselect(label="select columns",options=list(af.columns.values))
   ch5=co8.selectbox(label="test data ",options=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
 cds=None
+
 if recl == 'Classification':
-  mlclas(models,af,cds,ch3,ch4,ch5)
+  X_test,X_train=mlclas(models,af,cds,ch3,ch4,ch5)
   if ch3==True:
     cds1=cvse(models,af,ch4)
     
-    mlclas(models,af,cds1,ch3,ch4,ch5)
+    X_test,X_train=mlclas(models,af,cds1,ch3,ch4,ch5)
 if recl =='Regression':
     models=co5.selectbox(label='select a model',options=['Linear Regression','Lasso','Ridge','Decision Tree Regression',
                                                           'Random Forest Regression', 'Gradient Boosting Regression'])
     
     ch4=co6.multiselect(label="select columns",options=list(af.columns.values))
     ch5=co7.selectbox(label="test data ",options=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
-    mlreg(af,models,ch4,ch5)
+    X_test,X_train=mlreg(af,models,ch4,ch5)
+st.write('------------------------------------------------------------------------------------')
+try:
+ 
+ X_test_new=con(txt,ch4,recl)
+except Exception as e:
+  print(e)
+if len(ch4)!=0:   
+   X_test_new1=sclr(X_train,X_test,X_test_new)
+try:
+ if recl == 'Classification':
 
+      if len(ch4)==0:
+
+           tx=X_test_new
+
+           abcd.loc[abcd.index.max() + 1]=tx
+           af2=abcd
+
+           af2['status']=af2['status'].fillna(af2['status'].mode()[0])
+
+
+           af2=skewnes(af2,aa)
+           af3=lblencode(af2)
+           af3=onehotencode(af3)
+
+           X_test_new1=af3.tail(1)
+           X_test_new1= X_test_new1.drop(['status'], axis=1)
+           mlclas1(models,af3,cds,ch3,ch4,ch5,X_test_new1)
+      else:
+        mlclas1(models,af,cds,ch3,ch4,ch5,X_test_new1)
+ if recl =='Regression':
+      if len(ch4)==0:
+
+           tx=X_test_new
+
+           abcd.loc[abcd.index.max() + 1]=tx
+           af2=abcd
+
+           af2['selling_price']=af2['selling_price'].fillna(af2['selling_price'].mode()[0])
+
+           st.write(tx)
+           af2=skewnes(af2,aa)
+           af3=lblencode(af2)
+           af3=onehotencode(af3)
+
+           X_test_new1=af3.tail(1)
+           X_test_new1= X_test_new1.drop(['selling_price'], axis=1)
+           mlreg1(af3,models,ch4,ch5,X_test_new1)
+           
+      else:
+         mlreg1(af,models,ch4,ch5,X_test_new1) 
+except Exception as e:
+  st.warning(f':red[{e}]')
